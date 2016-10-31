cf85420bd907fc51f0d8cab453d5be1741df5a99
JackyOnline <435416847@qq.com>
2016-10-31 18:08:25 +0800

    10/28 role

--- controllers/role.js
+++ controllers/role.js
@@ -15,7 +15,32 @@ module.exports = function (app) {
 
     //查询角色权限列表
     router.get('/role_info/list', function (req, res,next) {
-        var sql = knex.select('*').from('role_info');
+        var sql = knex.select('*').from('role_info').where('status','!=',2)
+        var params = req.query;
+        if(params.seq_no){
+            sql = sql.where('seq_no', params.seq_no);
+        }
+        if(params.role_name){
+            sql = sql.where('role_name','like','%'+params.role_name+'%');
+        }
+        if(params.date1s){
+            sql = sql.where('date1','>=',params.date1s);
+        }
+        if(params.date1e){
+            sql = sql.where('date1','<=',params.date1e);
+        }
+        if(params.date2s){
+            sql = sql.where('date2','>=',params.date2s);
+        }
+        if(params.date2e){
+            sql = sql.where('date2','<=',params.date2e);
+        }
+        if(params.menu_id == 1){
+            sql = sql.where('menu_id',';');
+        }
+        if(params.menu_id == 2){
+            sql = sql.where('menu_id','!=',';');
+        }
         var infos={};
         sql.then(function (reply) {
             infos.totalSize = reply.length;
@@ -34,7 +59,53 @@ module.exports = function (app) {
         });
     });
 
-
-
-
+    router.get('/role_info/get', function (req, res, next) {
+        var seq_no = req.query.seq_no;
+        var sql = knex.select('*').from('role_info').where('seq_no',seq_no);
+        // 执行sql
+        sql.then(function (reply) {
+            res.json(reply);
+        }).catch(function (err) {
+            next(err);
+        });
+    });
+    //更新
+    router.post('/role_info/insert', function (req, res, next) {
+        var pro = req.body;
+        var seq_no = req.query.seq_no;
+        mydate = moment().format('YYYY-MM-DD HH:mm:ss');
+        pro.date2 = mydate;
+        var sql = knex('role_info').update(pro).where('seq_no',seq_no);
+        // 执行sql
+        sql.then(function (reply) {
+            res.json({data: reply});
+        }).catch(function (err) {
+            next(err);
+        });
+    });
+    //删除
+    router.post('/role_info/del', function (req, res, next) {
+        var seq_no = req.query.seq_no;
+        var sql = knex('role_info').update('status',2).where('seq_no',seq_no);
+        // 执行sql
+        sql.then(function (reply) {
+            res.json({data: reply});
+        }).catch(function (err) {
+            next(err);
+        });
+    });
+    //新建
+    router.post('/role_info/ins', function (req, res, next) {
+        var pro = req.body;
+        mydate = moment().format('YYYY-MM-DD HH:mm:ss');
+        pro.date1 = mydate;
+        pro.date2 = mydate;
+        var sql = knex('role_info').insert(pro);
+        // 执行sql
+        sql.then(function (reply) {
+            res.json({data: reply});
+        }).catch(function (err) {
+            next(err);
+        });
+    });
 };
--- public/js/Backstage/role_manage.js
+++ public/js/Backstage/role_manage.js
@@ -7,14 +7,29 @@ $(function () {
 
     var tempObj;
     var tempEmpObj;
+    var seq_no;
+    var new_seq_no;
     var $p_id = $("#role_manage_page");
+
+    //数据表格筛选处事件冒泡
+    $('.j_bubble').click(function (event) {
+        event.stopPropagation();
+    });
+
+    // 阻止回车触发表格填充事件
+    $('.j_bubble').keypress(function (e) {
+        e.stopPropagation();
+    });
+
     function init() {
         debugger;
         var params = { // 查询查询参数
-            seq_no: $p_id.find('#seq_q').val(), // 员工工号
-            role_name: $p_id.find('#name_q').val(), // 员工工号
-            date1: $p_id.find('#cTime').val(), // 员工工号
-            date2: $p_id.find('#uTime').val(), // 员工工号
+            seq_no: $p_id.find('#seq_q').val(), // 编号
+            role_name: $p_id.find('#name_n').val(), // 员工工号
+            date1s: $p_id.find('#search_s').val(), // 员工工号
+            date1e: $p_id.find('#search_e').val(), // 员工工号
+            date2s: $p_id.find('#new_s').val(), // 员工工号
+            date2e: $p_id.find('#new_e').val(), // 员工工号
             menu_id: $p_id.find('#status_q').val(), // 员工工号
         };
 
@@ -26,7 +41,7 @@ $(function () {
             {"col_id": "role_name"},
             {"col_id": "date1"},
             {"col_id": "date2"},
-            {"col_id": "menu_id"},
+            {"col_id": "seq_no"},
         ]; // 定义表格数据列id
         var aoColumnDefs = [{
             "colIndex": 0,
@@ -64,9 +79,12 @@ $(function () {
             "colIndex": 4,
             "html": function (data, type, full) {
                 if (!data) {
-                    return '<td><div class="text-center">未添加权限</div></td>';
                 }
-                return '<td><div class="text-center">已添加权限</div></td>';
+                if(full.menu_id == ';'){
+                    return '<td><div class="text-center">未添加权限</div></td>';
+                }else{
+                    return '<td><div class="text-center">已添加权限</div></td>';
+                }
             }
         }, {
             "colIndex": 5,
@@ -76,35 +94,26 @@ $(function () {
                     html = '<li><a class="change_stauts_down" href="javascript:void(0)">禁用</a></li>';
                 }
                 var retHtml = '';
-                if (full.user_no == 'admin') {
+                if (full.seq_no == '10000'|| full.seq_no == '10001'||full.seq_no == '10002') {
                     retHtml = retHtml + '<div class="drop-opt">' +
                         '<a href="javascript:;" id="dropLabel-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">详情<span class="icon-chevron-down"></span></a>' +
                         '<ul class="drop-cnt in" role="menu" aria-labelledby="dropLabel-1">' +
-                        '<li><a class="employee_resetpass" href="javascript:void(0)">重置密码</a></li>' +
+                        '<li><a class="employee_edit" href="javascript:void(0)" data-id="'+full.seq_no+'" data-toggle="modal">编辑</a></li>' +
                         '</ul>' +
                         '</div>';
                 } else {
-                    if (full.user_no == $("#emp_id").val()) {
-                        retHtml = retHtml + '<div class="drop-opt">' +
-                            '<a href="javascript:;" id="dropLabel-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">详情<span class="icon-chevron-down"></span></a>' +
-                            '<ul class="drop-cnt in" role="menu" aria-labelledby="dropLabel-1">' +
-                            '<li><a class="employee_edit" href="javascript:void(0)" data-toggle="modal">编辑</a></li>' +
-                            '<li><a class="employee_resetpass" href="javascript:void(0)">重置密码</a></li>' +
-                            '</ul>' +
-                            '</div>';
-                    } else {
-                        retHtml = retHtml + '<div class="drop-opt">' +
-                            '<a href="javascript:;" id="dropLabel-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">详情<span class="icon-chevron-down"></span></a>' +
-                            '<ul class="drop-cnt in" role="menu" aria-labelledby="dropLabel-1">' +
-                            '<li><a class="employee_edit" href="javascript:void(0)" data-toggle="modal">编辑</a></li>' + html +
-                            '<li><a class="employee_del" href="javascript:void(0)">删除</a></li>' +
-                            '<li><a class="employee_resetpass" href="javascript:void(0)">重置密码</a></li>' +
-                            '</ul>' +
-                            '</div>';
-                    }
+                    retHtml = retHtml + '<div class="drop-opt">' +
+                        '<a href="javascript:;" id="dropLabel-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">详情<span class="icon-chevron-down"></span></a>' +
+                        '<ul class="drop-cnt in" role="menu" aria-labelledby="dropLabel-1">' +
+                        '<li><a class="employee_edit" href="javascript:void(0)" data-id="'+full.seq_no+'" data-toggle="modal">编辑</a></li>' +
+                        '<li><a class="employee_del" href="javascript:void(0)" data-id="'+full.seq_no+'" data-toggle="modal">删除</a></li>' +
+                        '</ul>' +
+                        '</div>';
+
                 }
                 return retHtml;
             }
+
         }]; // 定义需要改变的列
 
         // 列表为空时的数据
@@ -112,8 +121,14 @@ $(function () {
         // 绘画表格
         TableAjax.drawTable(table_src, ajax_url, pageSize, aoColumns, aoColumnDefs, params, sZeroRecords, fnChangeDataCallback,fnDrawCallback);
     };
+    //搜索后列表重构
+    $("#employeeSeatchBut").on('click',function(){
+        init();
+    });
     //获取到数据的回调函数，需要更该时可定义
     function fnChangeDataCallback(data){
+        new_seq_no=data.data[0].seq_no;
+        debugger;
         return data;
     }
     //绘画完成之后的回调函数
@@ -127,12 +142,182 @@ $(function () {
         minimumResultsForSearch: Infinity   //隐藏下拉列表搜索框。Infinity可以数字替换，下拉列表项的最少条目数等于该数字时出现搜索框。
     }); // 美化下拉列表
 
+    // 动态绑定编辑事件
+    $(document).on("click", ".employee_edit", function () {
+        debugger;
+        seq_no = $(this).attr('data-id');
+        //查询详情 并自动填充
+        $.ajax({
+            type: "get",
+            url: "/role_info/get?seq_no="+seq_no,
+            dataType: "json",
+            data: {},
+            success: function (data) {
+                debugger;
+                $p_id.find("#idLab").val(data[0].seq_no);
+                $p_id.find("#nameLab").val(data[0].role_name);
+                var chk1 = document.getElementById('check1');
+                if(data[0].menu_id.indexOf(';1;') >= 0){
+                    chk1.checked = true;
+                }else{
+                    chk1.checked = false;
+                }
+                var chk2 = document.getElementById('check2');
+                if(data[0].menu_id.indexOf(';2;') >= 0){
+                    chk2.checked = true;
+                }else{
+                    chk2.checked = false;
+                }
+                var chk3 = document.getElementById('check3');
+                if(data[0].menu_id.indexOf(';3;') >= 0){
+                    chk3.checked = true;
+                }else{
+                    chk3.checked = false;
+                }
+                var chk4 = document.getElementById('check4');
+                if(data[0].menu_id.indexOf(';4;') >= 0){
+                    chk4.checked = true;
+                }else{
+                    chk4.checked = false;
+                }
+                var chk5 = document.getElementById('check5');
+                if(data[0].menu_id.indexOf(';5;') >= 0){
+                    chk5.checked = true;
+                }else{
+                    chk5.checked = false;
+                }
+
+            },
+            error: function (data) {
+                alert("系统错误");
+            }
+        });
+        // 显示成功对话框
+        $("#addAccountModal").modal('show');
+        //保存
+        $('#save_account').on('click', function () {
+            var chk6 = document.getElementById('check1');
+            var chk7 = document.getElementById('check2');
+            var chk8 = document.getElementById('check3');
+            var chk9 = document.getElementById('check4');
+            var chk10 = document.getElementById('check5');
+            debugger;
+            var menu_id = ';';
+            if(chk6.checked == true){
+                menu_id += 1+';';
+            }
+            debugger;
+            if(chk7.checked == true){
+                menu_id += 2+';';
+            }
+            if(chk8.checked == true){
+                menu_id += 3+';';
+            }
+            if(chk9.checked == true){
+                menu_id += 4+';';
+            }
+            if(chk10.checked == true){
+                menu_id += 5+';';
+            }
+            debugger;
+            var data = {
+                menu_id:menu_id,
+                role_name:$("#nameLab").val(),
+            };
+            $.ajax({
+                type: "post",
+                url: '/role_info/insert?seq_no='+seq_no,
+                dataType: "json",
+                data: data,
+                success: function (data) {
+                    debugger;
+                    alert("提交成功！");
+                    window.location.reload();
+                },
+                error: function (data) {
+                    alert("系统错误");
+                }
+            })
+        });
+    });
+    $(document).on("click", ".employee_del", function () {
+        $("#delAccountModal").modal('show');
+        $('#del_account').on('click', function () {
+            $.ajax({
+                type: "post",
+                url: '/role_info/del?seq_no='+seq_no,
+                dataType: "json",
+                data:{},
+                success: function (data) {
+                    debugger;
+                    alert("删除成功！");
+                    window.location.reload();
+                },
+                error: function (data) {
+                    alert("系统错误");
+                }
+            })
+        });
+    });
+    //添加用户弹窗
+    $('#addStaffModal').on('click', function () {
+        $('#newAccountModal').modal('show');
+        $('#new_account').on('click', function () {
+            var chka = document.getElementById('Ncheck1');
+            var chkb = document.getElementById('Ncheck2');
+            var chkc = document.getElementById('Ncheck3');
+            var chkd = document.getElementById('Ncheck4');
+            var chke = document.getElementById('Ncheck5');
+            debugger;
+            var menu_id = ';';
+            if(chka.checked == true){
+                menu_id += 1+';';
+            }
+            debugger;
+            if(chkb.checked == true){
+                menu_id += 2+';';
+            }
+            if(chkc.checked == true){
+                menu_id += 3+';';
+            }
+            if(chkd.checked == true){
+                menu_id += 4+';';
+            }
+            if(chke.checked == true){
+                menu_id += 5+';';
+            }
+            debugger;
+            new_seq = parseInt(new_seq_no);
+            var data = {
+                menu_id:menu_id,
+                role_name:$("#newnameLab").val(),
+                seq_no:new_seq+1,
+                status:1,
+            };
+            debugger;
+            $.ajax({
+                type: "post",
+                url: '/role_info/ins',
+                dataType: "json",
+                data: data,
+                success: function (data) {
+                    debugger;
+                    alert("提交成功！");
+                    window.location.reload();
+                },
+                error: function (data) {
+                    alert("系统错误");
+                }
+            })
+        });
+    });
     //弹出框居中
     $('.modal').on('show.bs.modal', function () {
         $(this).addClass('modal-outer');
     }).on('hidden.bs.modal', function () {
         $(this).removeClass('modal-outer');
     });
+
 })
 
 
